0f1a0225620934bf5b487eba6d8def671b607786
Chrysanthos Prodromou <chrysanthos.prodromou@gmail.com>
2020-10-18 11:55:02 +0300

    Hello world

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+    },
+    purge: [
+        './public/build/index.html',
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
